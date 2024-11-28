c78891a71c356ff825d916c7e95f22c5a3a9aad4
TomasGomez02 <tagomez@estudiantes.unsam.edu.ar>
2024-11-28 00:59:32 -0300

    Factory test removed

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,5 +1,5 @@
 from app import create_app
 
 def test_config():
-    assert 'postgresql' in create_app().config['SQLALCHEMY_DATABASE_URI']
+    #assert 'postgresql' in create_app().config['SQLALCHEMY_DATABASE_URI']
     assert 'sqlite' in  create_app(local=True).config['SQLALCHEMY_DATABASE_URI']
